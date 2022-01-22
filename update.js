d67202374d01473aebded2e03084e161187b8472
burnes <AlexBurnes@gmail.com>
2022-01-22 11:59:59 +0800

    version 0.1.3 fix update.js

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 // update.js
-// version 0.1.2
+// version 0.1.3
 
 const baseUrl      = 'https://raw.githubusercontent.com/AlexBurnes/btbsjs/master/';
 const fileList     =  "file-list.js";
@@ -12,15 +12,15 @@ export async function main(ns) {
 
 /** @param {import("Ns").NS } ns */
 async function update(ns) {
-    const host = ns.getHostName();
+    const host = ns.getHostname();
     await ns.wget(`${baseUrl}${fileList}`, fileList);
-    if (!ns.fileExists(host, fileList)) {
+    if (!ns.fileExists(fileList, host)) {
         ns.tprintf("failed get file list for update, %s/%s", baseUrl, fileList);
         return;
     }
 
     await ns.wget(`${baseUrl}${fileList}`, updateScript);
-    if (!ns.fileExists(host, updateScript)) {
+    if (!ns.fileExists(updateScript, host)) {
         ns.tprintf("failed get update-fetch script, %s/%s", baseUrl, updateScript);
         return;
     }
