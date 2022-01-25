1dee2259e4a28ee261dffcc8ab0f2ede9ced77f1
burnes <AlexBurnes@gmail.com>
2022-01-26 00:22:18 +0800

    version 0.2.0.5 fix update.js mv uploaded files to bk and delete, add logging for updater

--- update.js
+++ update.js
@@ -40,28 +40,42 @@ export async function main(ns) {
         return help(ns);
     }
     // do not use anything from current libraries
-    await update(ns);
+    const result = await update(ns);
+    if (!result) {
+        ns.tprintf("failed update");
+        return;
+    }
     ns.tprintf("done updating");
-    return;
 }
 
 /** @param {import("Ns").NS } ns */
 async function update(ns) {
     const host = ns.getHostname();
+    const update_success = true;
+    ns.tprintf("uploading core files");
     for(let i = 0; i < files_list.length; i++) {
         const file = files_list[i];
-        // before download file move to bk and delet
+        ns.tprintf("[%d/%d] move %s to bk_%s", i+1, files_list.length, file, file);
         ns.rm(`bk_${file}`);
+        if (ns.fileExists(`bk_%{file}`, host)) {
+            ns.tprintf("[%d/%d] filed delete bk_%s", i+1, files_list.length, file);
+            return false;
+        }
         if (ns.fileExists(file, host)) {
             ns.mv(host, file, `bk_${file}`);
         }
+        if (ns.fileExists(file, host)) {
+            ns.tprintf("[%d/%d] filed move file %s to bk_%s", i+1, files_list.length, file, file);
+            return false;
+        }
         await ns.wget(`${baseUrl}${file}`, file);
         if (!ns.fileExists(file, host)) {
-            ns.tprintf("failed get file for update %s/%s", baseUrl, file);
-            return;
+            ns.tprintf("[%d/%d] failed get file for update %s/%s", i+1, files_list.length, baseUrl, file);
+            return false;
         }
+        ns.tprintf("[%d/%d] %s uploaded", i+1, files_list.length, file);
     }
-
+    ns.tprintf("run update-fetch to complite updating");
     await ns.run("update-fetch.js", 1, baseUrl);
-
+    return true;
 }
