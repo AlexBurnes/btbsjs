efa6623260c83b9581de8d8cc3d356eab8907824
burnes <AlexBurnes@gmail.com>
2022-01-27 23:48:11 +0800

    add file settings

---
+++ h3ml/etc/h3ml-settings.js
@@ -0,0 +1,11 @@
+// this is a setting for h3ml scripts
+
+// default values
+
+export const settings = {
+    "logLevel"      : 1,
+    "debugLevel"    : 0,
+    "homeHost"      : 'devel-server',    // default home server, for working system must be defined as home
+    "ctrlHost"      : 'devel-ctr',       // name of control host
+    "hackHost"      : 'devel-hack',      // name of hack-server
+}
