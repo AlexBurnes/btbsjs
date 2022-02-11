bdf4fe4f6f7d10324202ed13eb47737a1e7e4308
burnes <AlexBurnes@gmail.com>
2022-02-11 19:47:57 +0800

    version 0.3.6.9 fix server-hack-speed module name

--- h3ml/sbin/server-hack-speed.js
+++ h3ml/sbin/server-hack-speed.js
@@ -1,5 +1,5 @@
-const Module  = '/h3ml/sbin/server-hack-batch.js';
-const Version = '0.3.6.5'; // update this every time when edit the code!!!
+const Module  = '/h3ml/sbin/server-hack-speed.js';
+const Version = '0.3.6.9'; // update this every time when edit the code!!!
 
 import {Constants}   from "/h3ml/lib/constants.js";
 import {Logger}      from "/h3ml/lib/log.js";
