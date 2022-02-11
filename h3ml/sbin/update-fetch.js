d470404b19e558436c8182f7b9e1522ef88d68e6
burnes <AlexBurnes@gmail.com>
2022-02-11 22:06:26 +0800

    version 0.3.6.26 fix setup

--- h3ml/sbin/update-fetch.js
+++ h3ml/sbin/update-fetch.js
@@ -1,6 +1,6 @@
 "use strict";
 const Module  = '/h3ml/sbin/update-fetch.js';
-const Version = '0.3.6.25'; // update this every time when edit the code!!!
+const Version = '0.3.6.26'; // update this every time when edit the code!!!
 
 /*
     update all scripts
@@ -131,7 +131,7 @@ async function update(l, baseUrl, host) {
 
         if (file == "/h3ml/sbin/worker-min.js") {
             // this file does not have module and version identites,ignore
-            l.g(1, "[%d/%d] got file %s success, %.2fGb", i+1, scriptFiles.length, file, module_version, scripts["get"](file));
+            l.g(1, "[%d/%d] got file %s success, %.2fGb", i+1, scriptFiles.length, file, scripts["get"](file));
             continue;
         }
 
